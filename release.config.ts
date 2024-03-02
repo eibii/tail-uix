d1bb6abccd6be3edf1a535b5a28f9f8057a82a8a
Edu <dev@eibii.com>
2024-03-02 19:28:54 -0300

    feat: Remoção de plugin
    BREAKING CHANGE: Removido plugin @semantic-release/npm do arquivo release.config.ts

--- release.config.ts
+++ release.config.ts
@@ -3,7 +3,6 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
     "@semantic-release/github",
     [
       "@semantic-release/git",
